18ef3f3980ad4fd859c8394a6b44ebf92558b206
Anthony Alberto <alberto.anthony@gmail.com>
2013-03-10 16:51:55 -0300

    Fixing indent

--- spec/features/cheats_spec.rb
+++ spec/features/cheats_spec.rb
@@ -1,21 +1,11 @@
 require 'spec_helper'
 describe "CheatsSpec" do
   before(:each) do
-  #  login_as Recruiter.first, scope: :recruiter, :run_callbacks => false
     visit shell_index_path
   end
-  #
-  #after(:each) do
-  #  Warden.test_reset!
-  #end
 
-  #it "allows shell access" do
-  #  page.should have_css "#shell_container"
-  #end
-  #
-  #
   CandidateDecorator::SECTIONS.each do |section|
-  it "displays #{section} modal box", js: true do
+    it "displays #{section} modal box", js: true do
       click_link section
       page.should have_css ".modal.#{section}"
     end
